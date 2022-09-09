5f3412ce97963db0d58252258556dcd5e609e8cf
Dae Hyeon <daehyeon1999@naver.com>
2022-09-09 09:25:28 +0900

    create screen

---
+++ lib/view/authentication/authentication_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:nolajyo_project/res/components/base_screen.dart';
+
+class AuthenticationScreen extends StatelessWidget {
+  const AuthenticationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const BaseScreen();
+  }
+}
